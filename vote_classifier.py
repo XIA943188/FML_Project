71754c959ca3919efeb38a7b40fe762ac28228c4
XIA943188 <50079399+XIA943188@users.noreply.github.com>
2020-01-09 19:59:53 +0800

    Add the weighted voting

--- vote_classifier.py
+++ vote_classifier.py
@@ -9,19 +9,49 @@ from data_processing import *
 
 
 class VoteClassifier:
-    def __init__(self, knn_n_neighbors=5, gb_n_estimators=100, rf_n_estimators='warn', rf_oob_score=False):
+    def __init__(self, knn_n_neighbors=5, gb_n_estimators=400, rf_n_estimators='warn', rf_oob_score=False):
         self.algorithms = [
-            KNeighborsClassifier(n_neighbors=knn_n_neighbors),
-            GradientBoostingClassifier(n_estimators=gb_n_estimators, random_state=10),
-            RandomForestClassifier(n_estimators=rf_n_estimators, oob_score=rf_oob_score, random_state=13)]
+            (KNeighborsClassifier(n_neighbors=knn_n_neighbors), 1.),
+            (GradientBoostingClassifier(n_estimators=gb_n_estimators, random_state=10), 10.),
+            (RandomForestClassifier(n_estimators=rf_n_estimators, oob_score=rf_oob_score, random_state=13), 1.)]
+        self.weights = []
+        self.labels = []
+
+    def weighted_voting(self, y_preds, weights):
+        y_pred = []
+        for i in range(len(y_preds[0])):
+            prediction = 0.
+            for j in range(len(weights)):
+                prediction += weights[j] * y_preds[j][i]
+            if abs(prediction - self.labels[0]) < abs(prediction - self.labels[1]):
+                y_pred.append(self.labels[0])
+            elif abs(prediction - self.labels[0]) > abs(prediction - self.labels[1]):
+                y_pred.append(self.labels[1])
+            else:
+                y_pred.append(y_vote[i])
+        return y_pred
 
     def fit(self, X, y):
-        for algorithm in self.algorithms:
+        self.labels = list(set(y))
+        y_preds, weights = [], []
+        for algorithm, weight in self.algorithms:
             algorithm.fit(X, y)
+            y_pred = algorithm.predict(X)
+            y_preds.append(y_pred)
+            weights.append(accuracy_calc(y, y_pred) * weight)
+        y_vote = [max_count([y_pred[i] for y_pred in y_preds]) for i in range(len(X))]
+        y_preds.append(y_vote)
+        weights.append(accuracy_calc(y, y_vote))
+        tot_weight = 0.
+        for weight in weights:
+            tot_weight += weight
+        self.weights = [weight/tot_weight for weight in weights]
 
     def predict(self, X):
-        predictions = [algorithm.predict(X) for algorithm in self.algorithms]
-        y = [max_count([y_pred[i] for y_pred in predictions]) for i in range(len(X))]
+        y_preds = [algorithm.predict(X) for algorithm, weight in self.algorithms]
+        y_vote = [max_count([y_pred[i] for y_pred in y_preds]) for i in range(len(X))]
+        y_preds.append(y_vote)
+        y = self.weighted_voting(y_preds, self.weights)
         return y
 
 
@@ -66,24 +96,40 @@ class Classifier:
         predictions = [self.voters[key].predict(X_test) for key in self.voters]
         y_pred = [max_count([y[i] for y in predictions]) for i in range(len(X_test))]
         y_pred_compare = [algorithm.predict(X_test) for algorithm in self.compare]
-        print(self.target_labels)
-        compare = [0, 0, 0]
-        count, tot = 0, len(X_test)
+        print("Target label: ", self.target_labels)
+        compare, target_compare = [0, 0, 0], [0, 0, 0]
+        count, target_count, tot, target_tot = 0, 0, len(X_test), 0
         for i in range(tot):
+            if y_test[i] in self.target_labels:
+                target_tot += 1
             if y_test[i] == y_pred[i]:
                 count += 1
+                if y_test[i] in self.target_labels:
+                    target_count += 1
             if y_pred_compare[0][i] == y_test[i]:
                 compare[0] += 1
+                if y_test[i] in self.target_labels:
+                    target_compare[0] += 1
             if y_pred_compare[1][i] == y_test[i]:
                 compare[1] += 1
+                if y_test[i] in self.target_labels:
+                    target_compare[1] += 1
             if y_pred_compare[2][i] == y_test[i]:
                 compare[2] += 1
-        accu = count / tot * 100
+                if y_test[i] in self.target_labels:
+                    target_compare[2] += 1
+        accu, target_accu = count / tot * 100, target_count / target_tot * 100
         accu_compare = [count_compare / tot * 100 for count_compare in compare]
-        print("Prediction completed. # correct: %d, # prediction: %d, total accuracy is %.1f%%." % (count, tot, accu))
+        accu_target_compare = [count_target_compare / target_tot * 100 for count_target_compare in target_compare]
+        print("Prediction completed. # test data: %d, "
+              "total accuracy is %.1f%%, target accuracy is %.1f%%." % (len(y_test), accu, target_accu))
         print("The accuracy of the simple model:")
-        print("\tKNeighborClassifier: %.1f%%\n\tGradientBoostingClassifier: %.1f%%\n\tRandomForestClassifier: %.1f%%"
-              % (accu_compare[0], accu_compare[1], accu_compare[2]))
+        print("\tKNeighborClassifier: total accuracy is %.1f%%, target accuracy is %.1f%%\n"
+              "\tGradientBoostingClassifier: total accuracy is %.1f%%, target accuracy is %.1f%%\n"
+              "\tRandomForestClassifier: total accuracy is %.1f%%, target accuracy is %.1f%%"
+              % (accu_compare[0], accu_target_compare[0],
+                 accu_compare[1], accu_target_compare[1],
+                 accu_compare[2], accu_target_compare[2]))
         return y_pred
 
 
